cb51138ce06622fad6d66135eea8d18c67408a42
1410400 <rakib.nsu.cse@gmail.com>
2018-07-30 13:16:39 +0600

    Controller Upates

---
+++ Shuffler/resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<link rel="stylesheet" href="style.css">
+<div class="jumbotron text-center">
+    <h1> Welcome to Shuffler</h1>
+<p><a class="btn-primary btn-lg"href="/login"role="button">Login</a> <a class="btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
+@endsection
