63f32507e80e5958e8f87670109b5c874562839b
Istiak Rahman <istiak.rahman@northsouth.edu>
2018-08-21 23:59:09 +0600

    Removing duplicate folder.

--- Shuffler/Shuffler/resources/views/pages/aboutus.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.indexapp')
-@section('content')
-<body style="background-image:url('Background.PNG')">
-<h1> {{$title}}</h1>
-<p>This is the about page</p>
-@endsection
--- Shuffler/Shuffler/resources/views/pages/services.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<body style="background-image:url('Background.PNG')">
-<h1>{{$title}}</h1>
-
-<p>this is the services page</p>
-@endsection
