db0b3d4cb37b577f67ecebf985d6706d2c104d2d
1410400 <rakib.nsu.cse@gmail.com>
2018-08-21 13:30:29 +0600

    Created Middleware for Navbar

---
+++ Shuffler/Shuffler/resources/views/places.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<body style="background-image:url('Background.PNG')">
+    <h1>{{$title}}</h1>
+    <p>This is the places page.</p>
+</body>
+@endsection
---
+++ Shuffler/resources/views/pages/service.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.indexapp')
+@section('content')
+<body style="background-image:url('Background.PNG')">
+<h1>{{$title}}</h1>
+
+<p>this is the services page</p>
+@endsection
