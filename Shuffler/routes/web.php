0e0f74079a043e673b3591e6fcf49b9df8e5a18f
Istiak Rahman <istiak.rahman@northsouth.edu>
2018-08-30 22:46:49 +0600

    Route fix.

--- Shuffler/routes/web.php
+++ Shuffler/routes/web.php
@@ -22,7 +22,5 @@ Auth::routes();
 Route::get('/places', 'PlacesController@index')->middleware('authenticated');
 Route::get('/dashboard', 'MapController@index')->middleware('authenticated');
 
-Route::get('/location', 'LocationController@index');
-
 Route::get('/redirect', 'Auth\LoginController@redirectToProvider');
 Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
